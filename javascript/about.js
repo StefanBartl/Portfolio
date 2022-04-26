afcded3307a19622e4650dcc60b8dfa4343b8609
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-04-26 14:52:32 +0200

    Folder structure update

--- javascript/about.js
+++ javascript/about.js
@@ -1,6 +1,6 @@
 //#region //?   Table of Content
 /*
-!                         Portfolio Main-Javascript-File
+!                         Portfolio about.js-Javascript-File
 ?                                      powered by
 !                                      Stefan Bartl
 !                        (WKDSteVIE / WKDMinerva)
@@ -9,7 +9,6 @@
 !                                     Table of content              
 ?                                       1) Language     
 ?                                       2) Audio
-?                                       3)  Open Jobs
 todo                 Javascript - what a wonderful language!
 */
 //#endregion
@@ -21,14 +20,14 @@ const language = localStorage.language || navigator.language;
 
 // set correct language
 general_translation(language);
-home_translation(language);
+about_translation(language);
 
 //#endregion
 
 //#region //?   Audio
 
 // get music file, load &  loop it
-const audio = new Audio("folder_audio/inkompetech_com/Late Night Radio.mp3");
+const audio = new Audio("audio/inkompetech_com/Late Night Radio.mp3");
 audio.load();
 audio.loop = true;
 
@@ -39,46 +38,39 @@ localStorage.Sound !== "on"
     : localStorage.Sound = "off"; 
     audio_settings();
 })
+
 // ! Audio settings for toggling, between playing & pausing audio and store in localStorage
 function audio_settings (){
-//console.log(`Èntered Audio-Settings Function in script.js.`);
+//console.log(`Èntered Audio-Settings Function in about.js.`);
 
 // load correct sound toggle image 
-localStorage.Sound !== "on" 
-    ? musicnote.src = "folder_graphics/folder_images/music_notes_dance/music_notes_dance_grey_bg.png"
-    : musicnote.src = "folder_graphics/folder_images/music_notes_dance/music_notes_dance_orange_bg.png";
-    
- // toggle sound text language
-if  (language === "de"){    // Deutsch
-localStorage.Sound !== "on" 
-    ? audio_text.innerText = "aus" 
-    : audio_text.innerText = "an";
-} else {                              //English
     localStorage.Sound !== "on" 
-    ? audio_text.innerText = "off" 
-    : audio_text.innerText = "on";
-}
+        ? musicnote.src = "graphics/images/music_notes_dance/music_notes_dance_grey_bg.png"
+        : musicnote.src = "graphics/images/music_notes_dance/music_notes_dance_orange_bg.png";
+    
+// toggle sound text language
+    if  (language === "de"){    // Deutsch
+    localStorage.Sound !== "on" 
+        ? audio_text.innerText = "aus" 
+        : audio_text.innerText = "an";
+    } else {                              //English
+        localStorage.Sound !== "on" 
+        ? audio_text.innerText = "off" 
+        : audio_text.innerText = "on";
+    }
 
 // toggle sound text colour
-localStorage.Sound !== "on"
-    ? audio_div.setAttribute("data-sound", "off")
-    : audio_div.setAttribute("data-sound", "on");
+    localStorage.Sound !== "on"
+        ? audio_div.setAttribute("data-sound", "off")
+        : audio_div.setAttribute("data-sound", "on");
 
 // Play or Pause Sound
     localStorage.Sound === "on" ?  audio.play() : audio.pause();
     
 //console.log(`Audio toggle completed, Sound setted to ${localStorage.Sound || 'off'}.`);
 };
-
+    
 // todo Invoke Audio
 audio_settings();
 
-
-//#endregion
-
-
-//#region //?   Open Jobs  
-/*
-todo    3-D Page concept ? 
-*/
 //#endregion
--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,15 @@
 //#region //?   Table of Content
 /*
-!                         Portfolio about.js-Javascript-File
+!                         Portfolio Main-Javascript-File
 ?                                      powered by
 !                                      Stefan Bartl
 !                        (WKDSteVIE / WKDMinerva)
-?                                            2021             
+?                                         Apr, 2022             
 ?                  ________________________________                                                                                                                                                                                                  
 !                                     Table of content              
 ?                                       1) Language     
 ?                                       2) Audio
+?                                       3)  Open Jobs
 todo                 Javascript - what a wonderful language!
 */
 //#endregion
@@ -20,14 +21,14 @@ const language = localStorage.language || navigator.language;
 
 // set correct language
 general_translation(language);
-about_translation(language);
+home_translation(language);
 
 //#endregion
 
 //#region //?   Audio
 
 // get music file, load &  loop it
-const audio = new Audio("folder_audio/inkompetech_com/Late Night Radio.mp3");
+const audio = new Audio("audio/inkompetech_com/Late Night Radio.mp3");
 audio.load();
 audio.loop = true;
 
@@ -38,39 +39,46 @@ localStorage.Sound !== "on"
     : localStorage.Sound = "off"; 
     audio_settings();
 })
-
 // ! Audio settings for toggling, between playing & pausing audio and store in localStorage
 function audio_settings (){
-//console.log(`Èntered Audio-Settings Function in about.js.`);
+//console.log(`Èntered Audio-Settings Function in script.js.`);
 
 // load correct sound toggle image 
-    localStorage.Sound !== "on" 
-        ? musicnote.src = "folder_graphics/folder_images/music_notes_dance/music_notes_dance_grey_bg.png"
-        : musicnote.src = "folder_graphics/folder_images/music_notes_dance/music_notes_dance_orange_bg.png";
+localStorage.Sound !== "on" 
+    ? musicnote.src = "graphics/images/music_notes_dance/music_notes_dance_grey_bg.png"
+    : musicnote.src = "graphics/images/music_notes_dance/music_notes_dance_orange_bg.png";
     
-// toggle sound text language
-    if  (language === "de"){    // Deutsch
+ // toggle sound text language
+if  (language === "de"){    // Deutsch
+localStorage.Sound !== "on" 
+    ? audio_text.innerText = "aus" 
+    : audio_text.innerText = "an";
+} else {                              //English
     localStorage.Sound !== "on" 
-        ? audio_text.innerText = "aus" 
-        : audio_text.innerText = "an";
-    } else {                              //English
-        localStorage.Sound !== "on" 
-        ? audio_text.innerText = "off" 
-        : audio_text.innerText = "on";
-    }
+    ? audio_text.innerText = "off" 
+    : audio_text.innerText = "on";
+}
 
 // toggle sound text colour
-    localStorage.Sound !== "on"
-        ? audio_div.setAttribute("data-sound", "off")
-        : audio_div.setAttribute("data-sound", "on");
+localStorage.Sound !== "on"
+    ? audio_div.setAttribute("data-sound", "off")
+    : audio_div.setAttribute("data-sound", "on");
 
 // Play or Pause Sound
     localStorage.Sound === "on" ?  audio.play() : audio.pause();
     
 //console.log(`Audio toggle completed, Sound setted to ${localStorage.Sound || 'off'}.`);
 };
-    
+
 // todo Invoke Audio
 audio_settings();
 
+
+//#endregion
+
+
+//#region //?   Open Jobs  
+/*
+todo    3-D Page concept ? 
+*/
 //#endregion
